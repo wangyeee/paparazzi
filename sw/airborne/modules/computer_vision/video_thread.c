9acb5996ee86a310a3c80d397e7b967de6f37306
Christophe De Wagter <dewagter@gmail.com>
2017-11-06 18:26:09 +0100

    Restructure bebop camera settings and ISP

--- sw/airborne/modules/computer_vision/video_thread.c
+++ sw/airborne/modules/computer_vision/video_thread.c
@@ -43,9 +43,9 @@
 // include board for bottom_camera and front_camera on ARDrone2, Bebop and Disco
 #include BOARD_CONFIG
 
-// Bebop and Disco can use the ISP (Image Signal Processors) to speed up thing
+// Bebop and Disco can use the ISP (Image Signal Processors) to speed up things
 #if defined(BOARD_BEBOP) || defined(BOARD_DISCO)
-#include "lib/isp/libisp.h"
+#include "boards/bebop/isp/libisp.h"
 #endif
 
 // Threaded computer vision
@@ -63,6 +63,12 @@ PRINT_CONFIG_VAR(VIDEO_THREAD_NICE_LEVEL)
 #define VIDEO_THREAD_MAX_CAMERAS 4
 #endif
 PRINT_CONFIG_VAR(VIDEO_THREAD_MAX_CAMERAS)
+
+#ifndef VIDEO_THREAD_VERBOSE
+#define VIDEO_THREAD_VERBOSE 0
+#endif
+
+#define printf_debug    if(VIDEO_THREAD_VERBOSE > 0) printf
 
 static struct video_config_t *cameras[VIDEO_THREAD_MAX_CAMERAS] = {NULL};
 
@@ -167,7 +173,7 @@ static bool initialize_camera(struct video_config_t *camera)
   if (camera->subdev_name != NULL) {
     // FIXME! add subdev format to config, only needed on Bebop/Disco(?) front camera so far
     if (!v4l2_init_subdev(camera->subdev_name, 0, camera->subdev_format, camera->sensor_size)) {
-      printf("[video_thread] Could not initialize the %s subdevice.\n", camera->subdev_name);
+      fprintf(stderr, "[video_thread] Could not initialize the %s subdevice.\n", camera->subdev_name);
       return false;
     }
   }
@@ -175,7 +181,7 @@ static bool initialize_camera(struct video_config_t *camera)
   // Initialize the V4L2 device
   camera->thread.dev = v4l2_init(camera->dev_name, camera->output_size, camera->crop, camera->buf_cnt, camera->format);
   if (camera->thread.dev == NULL) {
-    printf("[video_thread] Could not initialize the %s V4L2 device.\n", camera->dev_name);
+    fprintf(stderr, "[video_thread] Could not initialize the %s V4L2 device.\n", camera->dev_name);
     return false;
   }
 
@@ -209,7 +215,7 @@ bool add_video_device(struct video_config_t *device)
     cameras[i] = device;
 
     // Debug statement
-    printf("[video_thread] Added %s to camera array.\n", device->dev_name);
+    printf_debug("[video_thread] Added %s to camera array.\n", device->dev_name);
 
     // Successfully initialized
     return true;
@@ -228,7 +234,7 @@ static void start_video_thread(struct video_config_t *camera)
     // Start the streaming thread for a camera
     pthread_t tid;
     if (pthread_create(&tid, NULL, video_thread_function, (void *)(camera)) != 0) {
-      printf("[viewvideo] Could not create streaming thread for camera %s: Reason: %d.\n", camera->dev_name, errno);
+      fprintf(stderr, "[viewvideo] Could not create streaming thread for camera %s: Reason: %d.\n", camera->dev_name, errno);
       return;
     }
 #ifndef __APPLE__
@@ -248,7 +254,7 @@ static void stop_video_thread(struct video_config_t *device)
 
     // Stop the capturing
     if (!v4l2_stop_capture(device->thread.dev)) {
-      printf("[video_thread] Could not stop capture of %s.\n", device->thread.dev->name);
+      fprintf(stderr, "[video_thread] Could not stop capture of %s.\n", device->thread.dev->name);
       return;
     }
   }
@@ -260,6 +266,10 @@ static void stop_video_thread(struct video_config_t *device)
  */
 void video_thread_init(void)
 {
+  // Initialise all camera pointers to be NULL
+  for (int indexCameras = 0; indexCameras < VIDEO_THREAD_MAX_CAMERAS; indexCameras++) {
+    cameras[indexCameras] = NULL;
+  }
 }
 
 /**
